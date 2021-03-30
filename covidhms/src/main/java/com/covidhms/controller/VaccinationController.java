999364009757ad03dfa60a97a3f4a7a1daef26f4
ParimalShete <parimalmshete@gmail.com>
2021-03-30 19:38:56 +0530

    commit on 30

--- covidhms/src/main/java/com/covidhms/controller/VaccinationController.java
+++ covidhms/src/main/java/com/covidhms/controller/VaccinationController.java
@@ -5,10 +5,15 @@ import java.sql.Time;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.covidhms.model.Vaccine;
@@ -21,8 +26,7 @@ public class VaccinationController {
 	@Autowired
 	private VaccinationService service;
 	
-	@Autowired
-	private SmtpMailSender smtpMailSender;
+	
 	
 	private boolean flag=true;
 	
@@ -55,14 +59,10 @@ public class VaccinationController {
 		return vObj;
 	}
 	
-	@GetMapping("/vaccination")
-	@CrossOrigin(origins ="http://localhost:4200")
-	public String confirmAppointment() throws Exception{
-		System.out.println("In confirm appointment");
-		if(flag){
-			smtpMailSender.send("parimalmshete@gmail.com", "Test mail from Spring", "Congratulations!!! Your vaccination appointment is scheduled on 1 April 2021 !!");
-			return "Approved !!!";
-		}else
-			return "Not approved ! Try again !!";
+	@GetMapping("/vaccination/{v}")
+	//@CrossOrigin(origins ="http://localhost:4200")
+	public @ResponseBody ResponseEntity<String> confirmAppointment(@PathVariable("v") Vaccine v) throws Exception{
+		System.out.println("In confirm appointment 1");
+		return service.confirmationAppointment(v,flag);
 	}
 }
